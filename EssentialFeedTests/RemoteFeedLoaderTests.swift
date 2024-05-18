eef4da6ea1de5c38362215b7a6274d6d2d760ae0
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2024-05-18 07:52:12 -0300

    `RemoteFeedLoader` does not request data upon creation

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,25 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2024 PortoCode. All Rights Reserved.
+//
+
+import XCTest
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoader {
+    
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+}
