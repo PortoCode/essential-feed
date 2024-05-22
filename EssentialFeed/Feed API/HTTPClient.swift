cbeb2c34c20edfa5aa3bd199b0ac3d95a601ba2b
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2024-05-22 10:55:46 -0300

    Move HTTPClient to its own file

---
+++ EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,15 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2024 PortoCode. All Rights Reserved.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
