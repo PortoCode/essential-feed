15560002cdb8a782282b1561276830d463f10622
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2024-11-01 18:54:10 -0300

    Move `FeedCachePolicy` to its  own file

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,23 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2024 PortoCode. All Rights Reserved.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDys: Int {
+        return 7
+    }
+    
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDys, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
