70628c4baa22c29de711c91470dc9a269ee88a33
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2024-11-27 22:57:00 -0300

    Add `FeedImageCell` with storyboard connections

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,13 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2024 PortoCode. All Rights Reserved.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+}
