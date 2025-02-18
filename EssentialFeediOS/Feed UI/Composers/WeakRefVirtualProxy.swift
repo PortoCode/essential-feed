f9942d05f3cf24191a585debb410bb5b588c8d49
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-02-18 07:39:08 -0300

    Render error message on load feed error

--- EssentialFeediOS/Feed UI/Composers/WeakRefVirtualProxy.swift
+++ EssentialFeediOS/Feed UI/Composers/WeakRefVirtualProxy.swift
@@ -13,12 +13,17 @@ final class WeakRefVirtualProxy<T: AnyObject> {
     }
 }
 
+extension WeakRefVirtualProxy: FeedErrorView where T: FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel) {
+        object?.display(viewModel)
+    }
+}
+
 extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel) {
         object?.display(viewModel)
     }
 }
-
 
 extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
     func display(_ model: FeedImageViewModel<UIImage>) {
