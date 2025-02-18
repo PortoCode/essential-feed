bbb2ea2a47723859075529b981421b10bddd474f
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-02-18 07:51:23 -0300

    Move `ErrorView` to separate file

---
+++ EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,10 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2025 PortoCode. All Rights Reserved.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
