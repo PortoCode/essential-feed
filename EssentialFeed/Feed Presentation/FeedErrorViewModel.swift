7fec8505b219b86b38a238bec9fa0926c0271d49
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-02-22 18:40:11 -0300

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,16 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2025 PortoCode. All Rights Reserved.
+//
+
+public struct FeedErrorViewModel {
+    public let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
