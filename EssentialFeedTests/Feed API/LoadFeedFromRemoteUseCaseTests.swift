c9786236ae8a623cf38fd3f112125b0c8f5d1732
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-02-23 17:28:06 -0300

    Add `HTTPClientTask` so clients can cancel HTTP requests

--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -169,14 +169,19 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     // the spy's job is to capture the messages (invocations) in a clear and comprehensive way,
     // in order to check how many times the message was invoked, with what parameters and in which order
     private class HTTPClientSpy: HTTPClient {
+        private struct Task: HTTPClientTask {
+            func cancel() {}
+        }
+        
         private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
         
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
         
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
             messages.append((url, completion))
+            return Task()
         }
         
         func complete(with error: Error, at index: Int = 0) {
