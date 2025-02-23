4bfc2788cd1735ed8a251daaed4db7779cf058dd
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-02-23 16:14:11 -0300

    `RemoteFeedImageDataLoader` does not perform any URL request

---
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -0,0 +1,34 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2025 PortoCode. All Rights Reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+class RemoteFeedImageDataLoader {
+    init(client: Any) {
+        
+    }
+}
+
+class RemoteFeedImageDataLoaderTests: XCTestCase {
+    
+    func test_init_doesNotPerformAnyURLRequest() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy {
+        var requestedURLs = [URL]()
+    }
+}
