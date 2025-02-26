102bef9512011027a08589e196f05233db315027
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-02-26 18:03:51 -0300

    Extract `FeedLoaderStub` into a shared scope to remove duplication

---
+++ EssentialApp/EssentialAppTests/Helpers/FeedLoaderStub.swift
@@ -0,0 +1,18 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2025 PortoCode. All Rights Reserved.
+//
+
+import EssentialFeed
+
+class FeedLoaderStub: FeedLoader {
+    private let result: FeedLoader.Result
+    
+    init(result: FeedLoader.Result) {
+        self.result = result
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        completion(result)
+    }
+}
