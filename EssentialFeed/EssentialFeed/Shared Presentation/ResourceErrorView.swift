75d7efb26df3aa9b96e55dc122d52791e342d4d2
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-03-14 21:01:04 -0300

    Rename and move ResourceErrorView and ViewModel to Shared module

---
+++ EssentialFeed/EssentialFeed/Shared Presentation/ResourceErrorView.swift
@@ -0,0 +1,10 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2025 PortoCode. All Rights Reserved.
+//
+
+import Foundation
+
+public protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}
---
+++ EssentialFeed/EssentialFeed/Shared Presentation/ResourceErrorViewModel.swift
@@ -0,0 +1,16 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2025 PortoCode. All Rights Reserved.
+//
+
+public struct ResourceErrorViewModel {
+    public let message: String?
+    
+    static var noError: ResourceErrorViewModel {
+        return ResourceErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> ResourceErrorViewModel {
+        return ResourceErrorViewModel(message: message)
+    }
+}
