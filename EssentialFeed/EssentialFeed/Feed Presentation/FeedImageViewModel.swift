855a22f8eeef20cdcc3202821e899408d74a9cfe
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-03-15 00:37:01 -0300

    Remove unused FeedImagePresenter logic

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -3,12 +3,9 @@
 // Copyright © 2025 PortoCode. All Rights Reserved.
 //
 
-public struct FeedImageViewModel<Image> {
+public struct FeedImageViewModel {
     public let description: String?
     public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
     
     public var hasLocation: Bool {
         return location != nil
