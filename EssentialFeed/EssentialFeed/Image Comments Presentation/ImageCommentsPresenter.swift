a10f31ea136eb72432b6be96c82f0c827739a9ac
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-03-15 01:10:05 -0300

    Add image comments title

---
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -0,0 +1,16 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2025 PortoCode. All Rights Reserved.
+//
+
+import Foundation
+
+public final class ImageCommentsPresenter {
+    public static var title: String {
+        NSLocalizedString(
+            "IMAGE_COMMENTS_VIEW_TITLE",
+            tableName: "ImageComments",
+            bundle: Bundle(for: Self.self),
+            comment: "Title for the image comments view")
+    }
+}
