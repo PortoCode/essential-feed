1f3f92db0d030dd65543b3c6049d40a99f3e278c
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-03-19 23:33:01 -0300

    Extract endpoint URL creation to the API modules

---
+++ EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
@@ -0,0 +1,17 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2025 PortoCode. All Rights Reserved.
+//
+
+import Foundation
+
+public enum FeedEndpoint {
+    case get
+    
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case .get:
+            return baseURL.appendingPathComponent("/v1/feed")
+        }
+    }
+}
