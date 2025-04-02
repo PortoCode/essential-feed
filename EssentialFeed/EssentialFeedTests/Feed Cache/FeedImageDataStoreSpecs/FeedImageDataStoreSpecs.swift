0d75f09d71269253583b8bb31606cb1fc16e4526
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-01 23:15:19 -0300

    Extract reusable FeedImageDataStoreSpecs

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedImageDataStoreSpecs/FeedImageDataStoreSpecs.swift
@@ -0,0 +1,13 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2025 PortoCode. All Rights Reserved.
+//
+
+import Foundation
+
+protocol FeedImageDataStoreSpecs {
+    func test_retrieveImageData_deliversNotFoundWhenEmpty() throws
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() throws
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() throws
+    func test_retrieveImageData_deliversLastInsertedValue() throws
+}
